55061dc76cce42d2231dd2ae252b4390294f1175
santoshipatro12 <santoshipatra2006@gmail.com>
2025-10-31 08:13:45 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,22 @@
+let score ="33ab"
+
+console.log(typeof score);
+let valueInNumber = Number (score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// "33" =>33
+//"33abc"=> NAN not a num
+// true => 1
+let isLoggedIn ="santoshi"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true
+//"" => false
+//"santoshi" => true
+
+let num =33
+let stringNumber = String(num)
+console.log(typeof(stringNumber));
